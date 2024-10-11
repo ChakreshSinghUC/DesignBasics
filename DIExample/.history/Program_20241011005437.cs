bd68ff17787eff3ed001d9db62f61e68a883c3e6
Chakresh Singh <singhch@mail.uc.edu>
2024-10-11 01:15:34 -0400

    IoC and Dependency Injection Basics

---
+++ DIExample/.history/Program_20241011005437.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+public interface IEmailService
+{
+    void SendEmail(string message);
+}
+
+public class EmailService : IEmailService
+{
+    public void SendEmail(string message)
+    {
+        Console.WriteLine("Sending email: " + message);
+    }
+}
+
+public class Notification
+{
+    private readonly IEmailService _emailService;
+
+    public Notification(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    public void Send(string message)
+    {
+        _emailService.SendEmail(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup Dependency Injection
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IEmailService, EmailService>()
+            .AddSingleton<Notification>()
+            .BuildServiceProvider();
+
+        // Resolve dependencies
+        var notification = serviceProvider.GetService<Notification>();
+        notification.Send("Hello, Dependency Injection!");
+    }
+}
---
+++ DIExample/.history/Program_20241011010531.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+public interface IEmailService
+{
+    void SendEmail(string message);
+}
+
+public class EmailService : IEmailService
+{
+    public void SendEmail(string message)
+    {
+        Console.WriteLine("Sending email: " + message);
+    }
+}
+
+public class Notification
+{
+    private readonly IEmailService _emailService;
+
+    public Notification(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    public void Send(string message)
+    {
+        _emailService.SendEmail(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setup Dependency Injection
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IEmailService, EmailService>()
+            .AddSingleton<Notification>()
+            .BuildServiceProvider();
+
+        // Resolve dependencies
+        var notification = serviceProvider.GetService<Notification>();
+        if (notification != null)
+        {
+            notification.Send("Hello, Dependency Injection!");
+        }
+        else
+        {
+            Console.WriteLine("Notification service is not available.");
+        }
+    }
+}
